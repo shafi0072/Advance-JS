d38b196ec9b1e494f952731812206b9872cce783
shafidark <safi29317@gmail.com.com>
2020-08-07 08:02:08 +0600

    shafi

---
+++ TrueVsFlase.js
@@ -0,0 +1,17 @@
+// Falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0'
+// const name = "0";
+let name = {};
+console.log(name);
+if(name){
+    console.log('The condition is True');
+}
+else{
+    console.log('The condition is False');
+}
